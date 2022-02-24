e547d80f0dca09dfba926f376208cdbac9bf5f66
Jared Vandeventer <jaredvandeventer@CR000001-JV.hsd1.co.comcast.net>
2022-02-24 15:34:59 -0700

    changed url path in fetcher functions to get DLP scan alerts

--- FetcherForKeyVaultRecoverableTimer/Fetcher/__init__.py
+++ FetcherForKeyVaultRecoverableTimer/Fetcher/__init__.py
@@ -24,29 +24,33 @@ def main(mytimer: func.TimerRequest, msg: func.Out[typing.List[str]]) -> str:
     tenant_name = os.environ['tenant_name']
     token = os.environ['token']
     rule_name = os.environ['rule_name']
-    
-    SecurityPostureAssessmentResults = get_status(tenant_name, rule_name, token)
-    results = SecurityPostureAssessmentResults.get('data',[])
+
+    SecurityPostureAssessmentResults = get_status(
+        tenant_name, rule_name, token)
+    results = SecurityPostureAssessmentResults.get('data', [])
     violationsResourceID = []
     for data in results:
         violationsResourceID.append(data.get("resource_id"))
     if violationsResourceID:
         msg.set(violationsResourceID)
-        logging.info(f"Violated Resource ids enqueued to the remediation Storage Queue. The resources with violation are {violationsResourceID}")
-    logging.info(f"No violation for the rule {rule_name} on tenant {tenant_name}")
+        logging.info(
+            f"Violated Resource ids enqueued to the remediation Storage Queue. The resources with violation are {violationsResourceID}")
+    logging.info(
+        f"No violation for the rule {rule_name} on tenant {tenant_name}")
+
 
 def get_status(tenant_name, rule_name, token):
-    logging.info("Going to get the security assessment violations from the latest scan for the "+ tenant_name + " tenant and the rule is "+ rule_name)
-    get_url = "https://" + tenant_name +"/api/v1/security_assessment"
+    logging.info("Going to get the security assessment violations from the latest scan for the " +
+                 tenant_name + " tenant and the rule is " + rule_name)
+    get_url = "https://" + tenant_name + "/api/v1/alerts"
     payload = {
-        'token' : token, 
-        'cloud_provider' : CLOUD_PROVIDER, 
-        'rule_name' : rule_name, 
-        'status' : STATUS, 
-        'muted' : MUTED, 
-        'limit' : LIMIT, 
-        'skip' : 0
+        'token': token,
+        'cloud_provider': CLOUD_PROVIDER,
+        'rule_name': rule_name,
+        'status': STATUS,
+        'muted': MUTED,
+        'limit': LIMIT,
+        'skip': 0
     }
     res = requests.get(get_url, params=payload)
     return res.json()
-
