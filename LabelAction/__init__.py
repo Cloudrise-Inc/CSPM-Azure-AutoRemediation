01335ee28360c0deb2d3d43997e9e117dc4008e0
Jeremiah Sullenger <jsullenger@cloudrise.com>
2022-03-25 17:13:28 -0600

    Working label remediation

--- LabelAction/__init__.py
+++ LabelAction/__init__.py
@@ -3,7 +3,7 @@ import os
 
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobClient
+from azure.storage.blob import BlobClient, ContainerClient
 
 
 # Set up  logger
@@ -13,7 +13,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.getLevelName(LOG_LEVEL))
 
 container_label = os.environ.get("CONTAINER_LABEL", "")
-object_albel = os.environ.get("OBJECT_LABEL", "")
+blob_label = os.environ.get("BLOB_LABEL", "")
 
 
 def main(msg: func.QueueMessage) -> None:
@@ -21,9 +21,43 @@ def main(msg: func.QueueMessage) -> None:
     logger.info(f"Python queue trigger function processed a queue item: {message}")
 
     url, policy, profile, rule = message.split(",")
-    cred = DefaultAzureCredential()
-    client = BlobClient.from_blob_url(url, credential=cred)
-    props = client.get_blob_properties()
-    print(props.metadata)
+    if container_label is not None and len(container_label):
+        logger.info("Labeling container.")
+        tags = {"DLPScanAlert": render_label(container_label, policy, profile, rule)}
+        tag_container(os.path.dirname(url), **tags)
+    else:
+        logger.info("No container label defined, skipping label.")
 
-    # TODO: nstantiate container and blob client and apply labels
+    if blob_label is not None and len(blob_label):
+        logger.info("Labeling blob.")
+        tags = {"DLPScanAlert": render_label(container_label, policy, profile, rule)}
+        tag_blob(url, **tags)
+    else:
+        logger.info("No blob label defined, skipping label.")
+
+
+def tag_container(resource_id, **tags):
+    cred = DefaultAzureCredential()
+    client = ContainerClient.from_container_url(resource_id, credential=cred)
+    existing_tags = client.get_container_properties().metadata
+    new_tags = {**existing_tags, **tags}
+    client.set_container_metadata(new_tags)
+
+
+def tag_blob(resource_id, **tags):
+    cred = DefaultAzureCredential()
+    client = BlobClient.from_blob_url(resource_id, credential=cred)
+    existing_tags = client.get_blob_properties().metadata
+    new_tags = {**existing_tags, **tags}
+    client.set_blob_metadata(new_tags)
+
+
+def render_label(label, policy, profile, rule):
+    tags = {}
+    if "{policy}" in label:
+        tags["policy"] = policy
+    if "{profile}" in label:
+        tags["profile"] = profile
+    if "{rule}" in label:
+        tags["rule"] = rule
+    return label.format(**tags)
